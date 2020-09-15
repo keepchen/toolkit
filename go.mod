59a5b342f4cbaf245182e742748056c3732a3349
keepchen <keepchen2016@qq.com>
2020-09-15 18:10:04 +0800

    go mod

--- go.mod
+++
@@ -1,5 +0,0 @@
-module toolkit
-
-go 1.15
-
-require github.com/gin-gonic/gin v1.6.3
--- toolkit.go
+++ toolkit.go
@@ -1,19 +1,19 @@
 package toolkit
 
-// import (
-// 	"github.com/keepchen/toolkit/cors"
-// 	"github.com/keepchen/toolkit/file"
-// 	"github.com/keepchen/toolkit/strings"
-// )
+import (
+	"github.com/keepchen/toolkit/cors"
+	"github.com/keepchen/toolkit/file"
+	"github.com/keepchen/toolkit/strings"
+)
 
-// //Toolkit 结构体
-// type Toolkit struct {
-// 	cors.Cors
-// 	file.File
-// 	strings.Strings
-// }
+//Toolkit 结构体
+type Toolkit struct {
+	cors.Cors
+	file.File
+	strings.Strings
+}
 
-// //NewToolkit 实例化
-// func NewToolkit() *Toolkit {
-// 	return new(Toolkit)
-// }
+//NewToolkit 实例化
+func NewToolkit() *Toolkit {
+	return new(Toolkit)
+}
